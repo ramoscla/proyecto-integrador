12a2b4998d337b39119789e47fe9b1b3a76788e7
bianca pedemonte <bianncapede@gmail.com>
2023-06-13 22:01:03 -0300

    hice el js de search-results

---
+++ js/search-results.js
@@ -0,0 +1,50 @@
+let query = new URLSearchParams(window.location.search)
+let search = query.get('search')
+
+let proxy = 'https://api.allorigins.win/get?url='
+let scret_key = '4d3d37287d77a9575cc5d48b706a917a' // Secret Key
+let url = `https://api.deezer.com/search?q=${search}&secret_key=${scret_key}`
+
+let nodo_ul = document.querySelector('section ul')
+let nodo_imagen = document.querySelector('.cargando')
+
+nodo_imagen.src = 'img/cargando.gif'
+
+fetch(proxy + url)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(resultados) {
+        let datos = JSON.parse(resultados.contents)
+        return datos;
+    })
+    .then(function(datos) {
+        if (datos.data.length === 0) {
+            nodo_ul.innerHTML = '<h2>No hay resultados</h2>'
+        } else {
+            nodo_imagen.classList.add('ocultar')
+            nodo_imagen.classList.remove('cargando')
+            // nodo_imagen.style.display = 'none'
+            for (let i = 0; i < datos.data.length; i++) {
+                let id = datos.data[i].id;
+                let titulo = datos.data[i].title_short
+                let foto = datos.data[i].album.cover_xl
+                let cantante = datos.data[i].artist.name
+                
+                nodo_ul.innerHTML += `
+                        <li>
+                            <a href="detail-songs.html?id=${id}">
+                                <h3>${titulo}</h3>
+                                <img src="${foto}" alt="${titulo}">
+                                <a href="detail-artist.html?id=${datos.data[i].artist.id}">
+                                    ${cantante}
+                                </a>
+                            </a>
+                        </li>
+                    `
+            }
+        }
+    })
+    .catch(function(error) {
+        console.log(error)
+    })

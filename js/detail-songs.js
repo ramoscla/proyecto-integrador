2b7b7e3b6bcbdfdfb0d6e5bd597949af5c5f0235
bianca pedemonte <bianncapede@gmail.com>
2023-06-13 22:02:04 -0300

    modifique detail-songs js

--- js/detail-songs.js
+++ js/detail-songs.js
@@ -7,8 +7,8 @@ let url = `https://api.deezer.com/track/${id}&secret_key=${scret_key}`
 
 let nodo_imagen = document.querySelector('section img') // Foto album
 let nodo_h1 = document.querySelector('section h1') // Titulo cancion
-let nodo_h6 = document.querySelector('section h6') // Artista
-let nodo_p = document.querySelector('section p') // Album
+let nodo_h6_a = document.querySelector('section h6 a') // Artista
+let nodo_p_a = document.querySelector('section p a') // Album
 let nodo_button = document.querySelector('section button') // Agregar a playlist
 
 fetch(proxy + url)
@@ -22,8 +22,10 @@ fetch(proxy + url)
     .then(function(datos) {
         nodo_imagen.src = datos.album.cover_xl
         nodo_h1.textContent = datos.title_short
-        nodo_h6.textContent = datos.artist.name
-        nodo_p.textContent = datos.album.title
+        nodo_h6_a.textContent = datos.artist.name
+        nodo_h6_a.href = `detail-artist.html?id=${datos.artist.id}`
+        nodo_p_a.textContent = datos.album.title
+        nodo_p_a.href = `detail-album.html?id=${datos.album.id}`
 
         let lista_playlist_ids = localStorage.getItem('playlist_ids') // "['123','4123','12523','12']"
         if (JSON.parse(lista_playlist_ids) === null) { // ['123','4123','12523','12']
